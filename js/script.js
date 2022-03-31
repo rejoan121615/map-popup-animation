d038ef78ec882e90d4bc95f7a8740c5e85c14b56
Mohd Rejoan <wearecrx@gmail.com>
2022-03-31 12:05:16 +0600

    popup effect create

--- js/script.js
+++ js/script.js
@@ -1,29 +1,92 @@
 const map = document.querySelectorAll(".map-america");
 
-function PopUpActive() {
-    let text = document.querySelector(".text-america");
-  text.style.opacity = 1;
-  console.log(text)
+function AllMethods(txtClass, mapClass, type = "") {
+    let text = document.querySelector(txtClass);
+
+    // text popups
+    function PopUpActive() {
+        gsap.to(text, {
+            opacity: 1,
+            duration: 0.5,
+            y: "0px",
+            display: "block",
+        });
+    }
+
+    function PopUpDisable() {
+        gsap.to(text, {
+            opacity: 0,
+            duration: 0.5,
+            y: "25px",
+            display: "none",
+        });
+    }
+
+    // map color change
+    function MapColorActive(ele) {
+        gsap.to(ele, {
+            opacity: 1,
+            duration: 0.5,
+        });
+        PopUpActive();
+    }
+
+    function MapColorDisable(ele) {
+        gsap.to(ele, {
+            opacity: 0.5,
+            duration: 0.5,
+        });
+        PopUpDisable();
+    }
+
+    if (type === "array") {
+        mapClass.forEach((item, index) => {
+            // active color
+            document
+                .querySelector(item)
+              .addEventListener("mouseenter", (event) => {
+                  PopUpActive()
+                    gsap.to(mapClass, {
+                        opacity: 1,
+                        duration: 0.5,
+                    });
+                });
+            // disable color
+            document
+                .querySelector(item)
+              .addEventListener("mouseleave", (event) => {
+                  PopUpDisable()
+                    gsap.to(mapClass, {
+                        opacity: 0.5,
+                        duration: 0.5,
+                    });
+                });
+        });
+    } else {
+        const mapElement = document.querySelector(mapClass);
+
+        mapElement.addEventListener("mouseenter", (event) => {
+            MapColorActive(event.target);
+        });
+
+        mapElement.addEventListener("mouseleave", (event) => {
+            MapColorDisable(event.target);
+        });
+    }
 }
 
-function PopUpDisable() {
-    let text = document.querySelector(".text-america");
-    text.style.opacity = 0;
-}
+AllMethods("#america", ["#Vector_41", "#Vector_43"], "array");
+AllMethods("#china_2", "#china");
+AllMethods("#china_3", "#saudi-arabia");
+AllMethods("#spain_2", "#spain");
+AllMethods("#france_2", "#france");
+AllMethods("#england_2", "#england");
+AllMethods("#germany_2", "#germany");
+AllMethods("#netherlands_2", "#netherlands");
 
-map[0].addEventListener("mouseenter", (event) => {
-    event.target.style.opacity = 1;
-    PopUpActive();
-});
-
-map[0].addEventListener("mouseleave", (event) => {
-    event.target.style.opacity = 0.5;
-    PopUpDisable();
-});
-
-
-const data = [
-    {
-        map: ["map-america"],
-    },
-];
+// const data = [
+//     {
+//         map: "map-america",
+//         text: "text-china",
+//     },
+// ];
